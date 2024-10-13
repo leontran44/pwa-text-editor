d1564578975657cc23e3180670cf252b88a893ba
Leon <tranhoangquan44@gmail.com>
2024-10-13 12:19:29 +1100

    add logic methods for indexedDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,42 @@
 import { openDB } from 'idb';
+import { ids } from 'webpack';
 
 const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
+	openDB('jate', 1, {
+		upgrade(db) {
+			if (db.objectStoreNames.contains('jate')) {
+				console.log('jate database already exists');
+				return;
+			}
+			db.createObjectStore('jate', {
+				keyPath: 'id',
+				autoIncrement: true,
+			});
+			console.log('jate database created');
+		},
+	});
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+	console.log('PUT to the database');
+	const jateDb = await openDB('jate', 1);
+	const tx = jateDb.transaction('jate', 'readwrite');
+	const store = tx.objectStore('jate');
+	const request = store.put({ value: content });
+	const result = await request;
+	console.log('🚀 - data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+	console.log('GET from the database');
+	const jateDb = await openDB('jate', 1);
+	const tx = jateDb.transaction('jate', 'readonly');
+	const store = tx.objectStore('jate');
+	const request = store.getAll();
+	const result = await request;
+	console.log('🚀 - data retrieved from the database', result);
+	return result?.value;
+};
 
 initdb();
