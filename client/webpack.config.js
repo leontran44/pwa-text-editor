4ed049dea229ec227d9319447e8bae05e5d2dc0b
Leon <tranhoangquan44@gmail.com>
2024-10-13 14:00:10 +1100

    add plugins and CSS loaders

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -7,24 +7,67 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
+	return {
+		mode: 'development',
+		entry: {
+			main: './src/js/index.js',
+			install: './src/js/install.js',
+		},
+		output: {
+			filename: '[name].bundle.js',
+			path: path.resolve(__dirname, 'dist'),
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: './index.html',
+				title: 'Just Another Text Editor',
+			}),
 
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
+			new InjectManifest({
+				swSrc: './src-sw.js',
+				swDest: 'src-sw.js',
+			}),
+
+			new WebpackPwaManifest({
+				name: 'Just Another Text Editor',
+				fingerprints: false,
+				short_name: 'JATE',
+				description: 'A simple text editor',
+				background_color: '#ffffff',
+				theme_color: '#317EFB',
+				start_url: '/',
+				publicPath: '/',
+				icons: [
+					{
+						src: path.resolve('src/images/logo.png'),
+						sizes: [96, 128, 192, 256, 384, 512],
+						destination: path.join('assets', 'icons'),
+					},
+				],
+			}),
+		],
+
+		module: {
+			rules: [
+				{
+					test: /\.css$/i,
+					use: ['style-loader', 'css-loader'],
+				},
+				{
+					test: /\.m?js$/,
+					exclude: /node_modules/,
+					use: {
+						loader: 'babel-loader',
+						options: {
+							presets: ['@babel/preset-env'],
+							plugins: [
+								'@babel/plugin-proposal-object-rest-spread',
+								'@babel/transform-runtime',
+							],
+						},
+					},
+				},
+			],
+		},
+	};
 };
